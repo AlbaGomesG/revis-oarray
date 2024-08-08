64e0173f0dac62886472cfda77309dbb6f630c78
Giovanna Alba <giovannaalba.tds1.senai@gmail.com>
2024-08-08 11:53:14 -0300

    feat: add if exercise 03

---
+++ array03.js
@@ -0,0 +1,11 @@
+let metaLivros = 5;
+
+let livrosLidos = ["Harry Potter", "Para todos os garotos que eu já amei", "É assim que acaba"];
+
+let quantidade = metaLivros - livrosLidos.length; 
+
+for ( let i = 0; i < livrosLidos.length; i++); {
+    console.log(`Livros Lidos: ${livrosLidos[i]}`);
+}
+
+    
